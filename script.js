4ff7be99536daecc027314810ebdf372d1e8bb24
sarthaknp977 <sarthaknp977@gmail.com>
2024-03-30 22:30:33 +0545

    Added something

---
+++ script.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const cardsContainer = document.querySelector('.cards-container');
+    const cardCount = 40; // Number of cards
+  
+    for (let i = 1; i <= cardCount; i++) {
+      const cardHTML = `
+        <div class="cards">
+          <div class="card${i} cc" style="background-image:url('https://source.unsplash.com/275x340/?people&amp;${i}') ">
+            <div class="card-info">
+              <div class="username">Username</div>
+              <div class="logos">
+                <div class="bookmark"><img src="bookmark.svg" alt=""></div>
+                <div class="like"><img src="like.svg" alt=""></div>
+              </div>
+            </div>
+          </div>
+          <div class="post-info">
+            <div class="user-photo">
+              <img src="https://source.unsplash.com/275x340/?people&amp;${i}" alt="">
+              <span>UserName</span>
+            </div>
+            <div class="likesandviews">
+            <div class="likes">
+              <img src="like.svg" alt=""><span>${i}.${i+3}k</span></div>
+              <div class="views">
+              <img src="views.svg" alt=""><span>${i*i}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      `;
+      cardsContainer.insertAdjacentHTML('beforeend', cardHTML);
+    }
+  });
